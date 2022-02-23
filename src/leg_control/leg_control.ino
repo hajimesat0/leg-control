be47acc23bfad39938d97a3174b9777a08b95354
Hajime Sato <89377758+hajimesat0@users.noreply.github.com>
2022-02-24 07:00:16 +0900

    シリアル通信から指示を受信できるよう改造

--- src/leg_control/leg_control.ino
+++ src/leg_control/leg_control.ino
@@ -1,72 +1,78 @@
-/*
-  Arduino Starter Kit example
-  Project 5 - Servo Mood Indicator
-
-  This sketch is written to accompany Project 5 in the Arduino Starter Kit
-
-  Parts required:
-  - servo motor
-  - 10 kilohm potentiometer
-  - two 100 uF electrolytic capacitors
-
-  created 13 Sep 2012
-  by Scott Fitzgerald
-
-  https://store.arduino.cc/genuino-starter-kit
-
-  This example code is part of the public domain.
-*/
-
-// include the Servo library
 #include <Servo.h>
 
 Servo myServo1;  // create a servo object
 Servo myServo2;  // create a servo object
 
-int const potPin = A0; // analog pin used to connect the potentiometer
+int const recv_size_max = 16;
+char cli_string[recv_size_max] = {};
+int cli_string_index = 0;
+
+//int const potPin = A0; // analog pin used to connect the potentiometer
 int const waitTime = 125;
-int potVal;  // variable to read the value from the analog pin
+//int potVal;  // variable to read the value from the analog pin
 int angle;   // variable to hold the angle for the servo motor
+
+enum MODE {
+  STOP,
+  MOVE
+};
+MODE mode = STOP;
 
 void setup() {
   myServo1.attach(9); // attaches the servo on pin 9 to the servo object
   myServo2.attach(10); // attaches the servo on pin 9 to the servo object
   Serial.begin(9600); // open a serial connection to your computer
+  Serial.println("start");
 }
 
 void loop() {
+  int serial_readable_count = 0;
+  serial_readable_count = Serial.available();
+  if( serial_readable_count > 0 ) {
+    for( int i=0;i<serial_readable_count; i++ ) {
+      if( cli_string_index<recv_size_max ) {
+        cli_string[cli_string_index] = Serial.read();
+        if( cli_string[cli_string_index]=='\n' ) {
+          cli_string[cli_string_index] = '\0';
+          cli_string_index = 0;
+          break;
+        }
+        cli_string_index ++;
+        if( recv_size_max<=cli_string_index ) {
+          Serial.println("recv error");
+          cli_string_index = 0;
+          break;
+        }
+      }
+    }
+  }
 
-  myServo1.write(179);
+  String recv_string = String(cli_string);
 
-  delay(waitTime);
+  if( recv_string.startsWith("test",0)==true ) {
+    Serial.println("test reveiced");
+  } else if ( recv_string.startsWith("stop",0)==true ) {
+    mode = STOP;
+  } else if ( recv_string.startsWith("move",0)==true ) {
+    mode = MOVE;
+  }
 
-  myServo1.write(0);
+  if( mode==MOVE ) {
+    myServo1.write(179);
 
-  delay(waitTime);
+    delay(waitTime);
 
-  myServo2.write(0);
+    myServo1.write(0);
 
-  delay(waitTime);
+    delay(waitTime);
 
-  myServo2.write(179);
+    myServo2.write(0);
 
-  delay(waitTime);
-//  
-//  potVal = analogRead(potPin); // read the value of the potentiometer
-//  // print out the value to the Serial Monitor
-//  Serial.print("potVal: ");
-//  Serial.print(potVal);
-//
-//  // scale the numbers from the pot
-//  angle = map(potVal, 0, 1023, 0, 179);
-//
-//  // print out the angle for the servo motor
-//  Serial.print(", angle: ");
-//  Serial.println(angle);
-//
-//  // set the servo position
-//  myServo.write(angle);
-//
-//  // wait for the servo to get there
-//  delay(15);
+    delay(waitTime);
+
+    myServo2.write(179);
+
+    delay(waitTime);
+  }
+
 }
