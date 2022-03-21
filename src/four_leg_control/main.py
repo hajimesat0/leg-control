e722151dc12574b885b73cfec681e5722d3fa7ee
Hajime Sato <89377758+hajimesat0@users.noreply.github.com>
2022-03-21 02:03:30 +0000

    入力ステップへ動作するよう改造

--- src/four_leg_control/main.py
+++ src/four_leg_control/main.py
@@ -5,21 +5,18 @@ import Adafruit_PCA9685
 
 
 pwm = Adafruit_PCA9685.PCA9685(address=0x40)
-pwm.set_pwm_freq(50)
+pwm.set_pwm_freq(60)
 
 wait_time = 0.1
 angle = 150
 
 
 while True:
-    if 150 < angle:
-        angle = 150
-    else:
-        angle = 500
-    print( angle )
+    step = input()
+    print( 'step is ' + str(step) )
     for i in range(8):
-        pwm.set_pwm(i+8,0,angle)
-        time.sleep(wait_time)
+        pwm.set_pwm(i,0,step)
+        # pwm.set_pwm(i+8,0,step)
 
 # while True:
 #     pwm.set_pwm(15,0,150)
